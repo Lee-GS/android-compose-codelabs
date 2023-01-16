bf1c2f3a48e5ab6b87780ee30b8c530d87f8d024
Lee-GS <leegs4831@naver.com>
2023-01-17 00:08:20 +0900

    feat: Add fun WaterCounter() (#1)

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -27,28 +27,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-@Composable
-fun StatefulCounter(modifier: Modifier = Modifier) {
-    var count by rememberSaveable { mutableStateOf(0) }
-    StatelessCounter(
-        count = count,
-        onIncrement = { count++ },
-        modifier = modifier
-    )
-}
 
 @Composable
-fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(16.dp)) {
-        if (count > 0) {
-            Text("You've had $count glasses.")
-        }
-        Button(
-            onClick = onIncrement,
-            enabled = count < 10,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text("Add one")
-        }
-    }
+fun WaterCounter(modifier: Modifier = Modifier){
+    val count = 0
+    Text(text = "You've had $count glasses!", modifier=modifier.padding(16.dp))
 }
--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTaskItem.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTaskItem.kt
@@ -28,30 +28,3 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-@Composable
-fun WellnessTaskItem(
-    taskName: String,
-    checked: Boolean,
-    onCheckedChange: (Boolean) -> Unit,
-    onClose: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 16.dp),
-            text = taskName
-        )
-        Checkbox(
-            checked = checked,
-            onCheckedChange = onCheckedChange
-        )
-        IconButton(onClick = onClose) {
-            Icon(Icons.Filled.Close, contentDescription = "Close")
-        }
-    }
-}
