9f44acbfa54181b9a696fafa2856310070f38358
Lee-GS <leegs4831@naver.com>
2023-01-29 22:51:03 +0900

    feat: Add fun WellnessTask() (#15)

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTask.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTask.kt
@@ -19,3 +19,4 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
+data class WellnessTask(val id: Int, val label: String)
