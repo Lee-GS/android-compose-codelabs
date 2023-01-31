33fbc1320535a799bd46b6e45a98c93630030c45
Lee-GS <leegs4831@naver.com>
2023-01-31 23:26:15 +0900

    feat: Add fun WellnessViewModel() (#21)

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessViewModel.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessViewModel.kt
@@ -18,3 +18,15 @@ package com.codelabs.state
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 
+class WellnessViewModel : ViewModel(){
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun remove(item: WellnessTask){
+        _tasks.remove(item)
+    }
+}
+
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i")}
