0760f643404afdefbd2c014ab8ad9794f8be6d60
Lee-GS <leegs4831@naver.com>
2023-01-31 23:47:44 +0900

    feat: fix fun WellnessTasks() (#27)

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTask.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTask.kt
@@ -15,8 +15,9 @@
  */
 package com.codelabs.state
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-data class WellnessTask(val id: Int, val label: String, var checked: Boolean = false)
+data class WellnessTask(val id: Int, val label: String, val checked: MutableState<Boolean> = mutableStateOf(false))
