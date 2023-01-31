6e8464643862176f63e1369badfefed0cecb7986
Lee-GS <leegs4831@naver.com>
2023-01-31 23:02:24 +0900

    feat: Add fun getWellnessTasks() (#19)

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
@@ -17,6 +17,8 @@ package com.codelabs.state
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -25,6 +27,10 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 fun WellnessScreen(modifier: Modifier = Modifier){
     Column(modifier = modifier) {
         StatefulCounter()
+
+        val list = remember { getWellnessTasks().toMutableStateList()}
         WellnessTasksList()
     }
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i")}
