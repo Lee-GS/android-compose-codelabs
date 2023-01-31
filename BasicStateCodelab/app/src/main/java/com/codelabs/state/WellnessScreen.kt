28b0d983e54e76371bf2fe3a9cf146938c8fe0ee
Lee-GS <leegs4831@naver.com>
2023-01-31 23:35:20 +0900

    feat: fix data class WellnessTask (#23)

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
@@ -15,6 +15,7 @@
  */
 package com.codelabs.state
 
+import android.system.Os.remove
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -24,13 +25,16 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier){
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+){
     Column(modifier = modifier) {
         StatefulCounter()
 
-        val list = remember { getWellnessTasks().toMutableStateList()}
-        WellnessTasksList()
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCloseTask = { task -> wellnessViewModel.remove(task)}
+        )
     }
 }
-
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i")}
--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTask.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTask.kt
@@ -19,4 +19,4 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-data class WellnessTask(val id: Int, val label: String)
+data class WellnessTask(val id: Int, val label: String, var checked: Boolean = false)
